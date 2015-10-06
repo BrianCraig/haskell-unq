3e4c868d4cd2d0245dc9fcf39c5492c28802e206
Brian Craig <briancraigok@gmail.com>
2015-10-06 09:21:27 -0300

    implementado Stack

---
+++ Stack.hs
@@ -0,0 +1,21 @@
+module Stack(Stack, emptyS, isEmptyS, push, top, pop) where
+
+data Stack a = S [a] deriving (Show)
+
+emptyS :: Stack a
+emptyS = S []
+
+isEmptyS :: Stack a -> Bool
+isEmptyS (S []) = True
+isEmptyS _ = False
+
+push :: a -> Stack a -> Stack a
+push a (S b) = S ([a] ++ b)
+
+top :: Stack a -> a
+top (S []) = error "NO HAY ELEMENTOS"
+top (S (x:_)) = x
+
+pop :: Stack a -> Stack a
+pop (S []) = error "NO SE PUEDE QUITAR UN ELEMENTO DE UN STACK VACIO"
+pop (S (_:xs)) = S xs
